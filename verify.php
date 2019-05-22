e9dd2ee70e97a02833e73aa6dab2f9284cfe5bef
maygive <46147791+maygive@users.noreply.github.com>
2019-05-22 22:58:53 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+$access_token = '8pBMkUfKCUV28mdtUPN32BcZyGFF4WNDged/rfVDW8pl0v7z+6pH696RarAC3Dq2hfd2Bfq4eW6gUsyH2b0n8JO3Ab99uMIyaA+tlVmgTib02DSlcTuV173EIeUvFSR2eh70cZAZ22a4AVzNGDqVtQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
